ea5331bbb36879ced16d1ab258158966a7d82f7b
youguifang <guifang.you@tcl.com>
2021-09-28 16:07:17 +0800

    update

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-const importLazy = require('import-lazy')(require); // 懒加载模块
-const latestVersion = importLazy('latest-version'); // 用于获取最新版本的 npm 包
-const semver = importLazy('semver'); // 语义化版本工具
+const latestVersion = require('latest-version'); // 用于获取最新版本的 npm 包
+const semver = require('semver'); // 语义化版本工具
 
 class MyUpdateNotifier {
   constructor(options) {
